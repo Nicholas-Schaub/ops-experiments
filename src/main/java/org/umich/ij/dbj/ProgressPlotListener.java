38c88ab0d650142f8b5145d0939b660ecf3906d0
Nick Schaub <Nick Schaub@10.0.0.8>
2017-06-06 23:35:10 -0400

    Added XChart for live plotting.

--- src/main/java/org/umich/ij/dbj/ProgressPlotListener.java
+++ src/main/java/org/umich/ij/dbj/ProgressPlotListener.java
@@ -1,9 +1,10 @@
 package org.umich.ij.dbj;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.JFrame;
 
 import org.deeplearning4j.earlystopping.EarlyStoppingConfiguration;
 import org.deeplearning4j.earlystopping.EarlyStoppingResult;
@@ -11,7 +12,11 @@ import org.deeplearning4j.earlystopping.listener.EarlyStoppingListener;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.Model;
 import org.deeplearning4j.optimize.api.TrainingListener;
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.style.Styler.ChartTheme;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.python.modules.time.Time;
 import org.scijava.log.LogService;
 
 import ij.gui.Plot;
@@ -21,10 +26,12 @@ public class ProgressPlotListener
 implements TrainingListener, EarlyStoppingListener {
 
 	// Containers for plotting values
-	private ArrayList<Long> epochInd = new ArrayList<Long>();
-	private ArrayList<Double> trainScore = new ArrayList<Double>();
-	private ArrayList<Double> testScore = new ArrayList<Double>();
+	private List<Double> epochInd = new ArrayList<Double>();
+	private List<Double> trainScore = new ArrayList<Double>();
+	private List<Double> testScore = new ArrayList<Double>();
 	
+	// Charting objects
+	private XYChart progressChart;
 	
 	private long trainIterCount = 0;
 	private long totalTrainIter = 0;
@@ -47,7 +54,7 @@ implements TrainingListener, EarlyStoppingListener {
 		this.logFrequency = logFrequency;
 		trainScore.add((double) 0);
 		testScore.add((double) 0);
-		epochInd.add((long) 1);
+		epochInd.add((double) 1);
 	}
 
 	@Override
@@ -98,21 +105,40 @@ implements TrainingListener, EarlyStoppingListener {
 		}
 		
 		log.info("Drawing train scores...");
-		plot = new Plot("Training Progress","Epoch","Objective",epochs,train,Plot.CROSS);
-		plot.setLimits(epochInd.get(0)-1, epochInd.get(epochInd.size()-1)+1, Collections.min(trainScore)*1.1, Collections.max(trainScore)*0.9);
+		if (progressChart==null) {	
+			progressChart = new XYChart(800,600,ChartTheme.GGPlot2);
+			progressChart.setTitle("Training Progress");
+			progressChart.setXAxisTitle("Epochs");
+			progressChart.setYAxisTitle("Objective");
+			progressChart.addSeries("Train Data", epochs, train);
+			progressChart.addSeries("Test Data", epochs, test);
+			XChartPanel<XYChart> chartPanel = new XChartPanel<XYChart>(progressChart);
+			JFrame frame = new JFrame("Training Progress");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(chartPanel);
+			frame.setVisible(true);
+			Time.sleep(500);
+		} else {
+			progressChart.updateXYSeries("Train Data", epochs, train, null);
+			progressChart.updateXYSeries("Test Data", epochs, test,null);
+			Time.sleep(500);
+		}
+
+		//plot = new Plot("Training Progress","Epoch","Objective",epochs,train,Plot.CROSS);
+		//plot.setLimits(epochInd.get(0)-1, epochInd.get(epochInd.size()-1)+1, Collections.min(trainScore)*1.1, Collections.max(trainScore)*0.9);
 		//log.info("Drawing train scores...");
 		//plot.setColor(Color.BLACK);
 		//plot.addPoints(epochs, train,Plot.LINE);
-		plot.draw();
-		log.info("Drawing test scores...");
+		//plot.draw();
+		//log.info("Drawing test scores...");
 		//plot.setColor(Color.RED);
-		plot.addPoints(epochs, test,Plot.CROSS);
-		if (plotWin==null) {
-			plotWin = plot.show();
-		} else {
-			plotWin.drawPlot(plot);
-		}
-		
+		//plot.addPoints(epochs, test,Plot.CROSS);
+		//if (plotWin==null) {
+		//	plotWin = plot.show();
+		//} else {
+		//	plotWin.drawPlot(plot);
+		//}
+		//plotWin.draw();
 		epochInd.add(epochInd.get(epochInd.size()-1)+1);
 		trainIterCount = 0;
 	}
